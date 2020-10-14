1a51a9820b89206020f91bfbec5aaa01dc9dcb55
Victor Ciornii <ciorniivictor96@gmail.com>
2020-10-15 00:48:47 +0300

    first commit

---
+++ My-Gulp-Project-master-2/src/assets/js/main.js
@@ -0,0 +1,69 @@
+"use strict";
+const d = document;
+d.addEventListener("DOMContentLoaded", function (event) {
+
+    // --------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------- Preloader
+    var preloader = d.querySelector('.preloader');
+    if (preloader) {
+        setTimeout(function () {
+            preloader.classList.add('show');
+
+            setTimeout(function () {
+                d.querySelector('.loader-element').classList.add('hide');
+            }, 200);
+        }, 1000);
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------  Slide Down a NavBar on Scroll
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        const navbar = document.querySelector(".navbar--scroll");
+        if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 500) {
+            navbar.style.top = "0";
+        } else {
+            navbar.style.top = "-80px";
+        }
+    }
+
+
+
+    // --------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------- Mobile Menu
+
+    let menuToggle = document.querySelectorAll('.menu-toggle svg'),
+        menu = document.querySelector('.mmenu');
+
+    menuToggle.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('mmenu-hide');
+        });
+    });
+
+
+    // --------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------- Smooth scroll
+
+    let scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true
+    });
+
+
+    const upElem = document.querySelector('.pageup');
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 500) {
+            upElem.classList.add('animated', 'fadeIn');
+            upElem.classList.remove('fadeOut');
+        } else {
+            upElem.classList.add('fadeOut');
+            upElem.classList.remove('fadeIn');
+        }
+    });
+});
