d895e27d17a797beb7c85394b50e3939ac685c0b
Victor Ciornii <ciorniivictor96@gmail.com>
2020-10-15 21:48:26 +0300

    refactoring

---
+++ My-Gulp-Project-master/src/js/main.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    
+});
+
+window.addEventListener('load', () => {
+    document.getElementById("preloader").style.display = "none";
+});
