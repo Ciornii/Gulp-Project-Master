c5bdc604c08d38f9d4b744aa2890e19fefc13e58
Victor Ciornii <ciorniivictor96@gmail.com>
2020-12-29 17:19:54 +0200

    scroll up

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,69 @@
-import $ from 'jquery'; window.jQuery = $; window.$ = $;
-import 'owl.carousel';
-import SmoothScroll from 'smooth-scroll';
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+import "owl.carousel";
+import SmoothScroll from "smooth-scroll";
 
-import moduleTest from './modules/moduleTest';
+import moduleTest from "./modules/moduleTest";
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    'use strict';
+  "use strict";
 
-    moduleTest('test-title');
-   
-    // --------------------------------------------------------------------------------------------------------
-    // ------------------------------------------------------------------- Preloader
-    const preloader = document.querySelector('.preloader');
-    if (preloader) {
-        setTimeout(function () {
-            preloader.classList.add('show');
+  moduleTest("test-title");
 
-            setTimeout(function () {
-                document.querySelector('.loader-element').classList.add('hide');
-            }, 200);
-        }, 1000);
+  // --------------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------- Preloader
+  const preloader = document.querySelector(".preloader");
+  if (preloader) {
+    setTimeout(function () {
+      preloader.classList.add("show");
+
+      setTimeout(function () {
+        document.querySelector(".loader-element").classList.add("hide");
+      }, 200);
+    }, 1000);
+  }
+
+  // --------------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------- Mobile Menu
+
+  let menuToggle = document.querySelectorAll(".menu-toggle svg"),
+    menu = document.querySelector(".mmenu");
+
+  menuToggle.forEach((item) => {
+    item.addEventListener("click", () => {
+      menu.classList.toggle("mmenu-hide");
+    });
+  });
+
+  // --------------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------- Smooth scroll
+
+  let scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+  });
+
+  const upElem = document.querySelector(".pageUp");
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 500) {
+      upElem.style.visibility = "visible";
+      upElem.classList.add("animated", "fadeIn");
+      upElem.classList.remove("fadeOut");
+    } else {
+      upElem.classList.add("fadeOut");
+      upElem.classList.remove("fadeIn");
+      upElem.style.visibility = "hidden";
     }
+  });
 
-
-    // --------------------------------------------------------------------------------------------------------
-    // ------------------------------------------------------------------- Mobile Menu
-
-    let menuToggle = document.querySelectorAll('.menu-toggle svg'),
-        menu = document.querySelector('.mmenu');
-
-    menuToggle.forEach(item => {
-        item.addEventListener('click', () => {
-            menu.classList.toggle('mmenu-hide');
-        });
-    });
-
-
-    // --------------------------------------------------------------------------------------------------------
-    // ------------------------------------------------------------------- Smooth scroll
-
-    let scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 500,
-        speedAsDuration: true
-    });
-
-
-    const upElem = document.querySelector('.pageUp');
-
-    window.addEventListener('scroll', () => {
-        if (document.documentElement.scrollTop > 500) {
-            upElem.classList.add('animated', 'fadeIn');
-            upElem.classList.remove('fadeOut');
-        } else {
-            upElem.classList.add('fadeOut');
-            upElem.classList.remove('fadeIn');
-        }
-    });
-
-    // --------------------------------------------------------------------------------------------------------
-    // ------------------------------------------------------------------- Slider (page2)
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        items: 1
-    });
+  // --------------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------- Slider (page2)
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+  });
 });
