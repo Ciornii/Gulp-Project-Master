86108d0e68ca818df66fb6417b89bcac68996ad9
Victor Ciornii <ciorniivictor96@gmail.com>
2021-08-09 10:34:20 +0300

    Update sliders.js

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,14 +1,25 @@
 const sliders = () => {
   // --------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------  Homepage Sliders
-  const swiper = new Swiper(".swiper-container", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  const hero = new Swiper('.hero', {
+    slidesPerView: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    speed: 900,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: true,
+      autoplayHoverPause: true,
     },
+    lazy: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
     },
   });
 };
